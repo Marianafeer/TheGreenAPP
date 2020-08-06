d9b58fbc09625c2ffc7bff1040e60ac890d7228d
Mariana Rodríguez <mariana@Marianas-iMac.local>
2020-08-06 09:43:47 -0700

    init

---
+++ TheGreenAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGreenAPP
+//
+//  Created by Mariana Rodríguez on 8/6/20.
+//  Copyright © 2020 Mariana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
