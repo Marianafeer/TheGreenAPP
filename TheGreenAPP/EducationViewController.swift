d8241254fb817ea6cfb1b4e9895f2a67aefa0237
Mariana Rodríguez <mariana@Marianas-iMac.local>
2020-08-06 10:38:53 -0700

    skeleton done

---
+++ TheGreenAPP/EducationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EducationViewController.swift
+//  TheGreenAPP
+//
+//  Created by Mariana Rodríguez on 8/6/20.
+//  Copyright © 2020 Mariana. All rights reserved.
+//
+
+import UIKit
+
+class EducationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
